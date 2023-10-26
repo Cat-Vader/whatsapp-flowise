21dec322434a9969411b02d0a0176674ee67d914
Daniel Omusula <94360277+Cat-Vader@users.noreply.github.com>
2023-10-26 13:07:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const wa = require('@open-wa/wa-automate');
-const axios = require('axios');
+const wa = require('@open-wa/wa-automate');  // Import the WhatsApp automation library
+const axios = require('axios');  // Import the Axios library for HTTP requests
 
-// Números permitidos na lista branca (no formato 5511993589393@c.us)
-const whitelistedNumbers = [
-  '551234567890@c.us',
-  '551111111111@c.us',
-  '5511912345678@c.us' // Adicione seu número aqui
-];
-
+// Initialize a new WhatsApp session
 wa.create({
   sessionId: "COVID_HELPER",
   multiDevice: true,
@@ -19,54 +13,41 @@ wa.create({
   logConsole: false,
   popup: true,
   qrTimeout: 0,
-}).then(client => start(client));
+}).then(client => start(client));  // Start the client when initialization is complete
 
 function start(client) {
-  // Evento de recebimento de mensagem
+  // Event listener for incoming messages
   client.onMessage(async message => {
-    console.log('Mensagem recebida:', message.body);
-    console.log('Remetente:', message.from);
+    console.log('Received message:', message.body);
+    console.log('Sender:', message.from);
 
-    // Verificar se o número está na lista branca
-    if (isNumberWhitelisted(message.from)) {
-      console.log('Número autorizado.');
-
-      if (message.body === 'Hi') {
-        await client.sendText(message.from, '👋 Olá!');
-        console.log('Resposta enviada: 👋 Olá!');
-      } else {
-        // Enviar a pergunta para o endpoint da API
-        const question = message.body;
-        const token = 'pOZiOWTf4aDiBD2PinQyX9nEjXstIPeGecqUx2onR/E='; // Substitua pelo seu token de autorização
-
-        try {
-          const response = await axios.post('http://192.168.15.8:3000/api/v1/prediction/2f3522c3-1e9f-4f2e-a411-f34303e98cd2', {
-            question: question
-          }, {
-            headers: {
-              Authorization: `Bearer ${token}`
-            }
-          });
-
-          const answer = response.data; // Obter a resposta da API
-
-          await client.sendText(message.from, answer);
-          console.log('Resposta enviada:', answer);
-        } catch (error) {
-          console.error('Erro na solicitação à API:', error);
-          await client.sendText(message.from, 'Desculpe, ocorreu um erro ao processar sua pergunta.');
-          console.log('Resposta de erro enviada: Desculpe, ocorreu um erro ao processar sua pergunta.');
-        }
-      }
+    if (message.body === 'Hi') {  // If the message is "Hi"
+      await client.sendText(message.from, '👋 Hello!');  // Send a greeting
+      console.log('Response sent: 👋 Hello!');
     } else {
-      console.log('Número não autorizado.');
-      await client.sendText(message.from, 'Desculpe, você não está autorizado a interagir com este chatbot.');
-      console.log('Resposta de autorização negada enviada.');
+      // Send the message content to an API endpoint
+      const question = message.body;
+      const token = 'your-authorization-token';  // Replace with your authorization token
+
+      try {
+        // Make a POST request to the API endpoint
+        const response = await axios.post('http://your-api-endpoint.com', {
+          question: question
+        }, {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+
+        const answer = response.data;  // Get the response from the API
+        await client.sendText(message.from, answer);  // Send the API response as a message
+        console.log('Response sent:', answer);
+      } catch (error) {
+        console.error('API request error:', error);
+        // Send an error message if the API request fails
+        await client.sendText(message.from, 'Sorry, there was an error processing your question.');
+        console.log('Error response sent: Sorry, there was an error processing your question.');
+      }
     }
   });
-}
-
-function isNumberWhitelisted(number) {
-  // Verificar se o número está presente na lista branca
-  return whitelistedNumbers.some(whitelistedNumber => whitelistedNumber === number);
 }
